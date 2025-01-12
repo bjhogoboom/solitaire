33e087d6b43df84b583e5bed3070e18e5c3da94b
bjhogoboom <bjhogoboom@gmail.com>
2025-01-11 16:24:19 -0800

    Use card stats texture regardless of whether it's newly initialized

--- card.gd
+++ card.gd
@@ -6,8 +6,7 @@ var stats: CardStats
 func _ready() -> void:
 	if !stats:
 		stats = CardStats.random_card_stats()
-		texture = stats.texture()
-		
+	texture = stats.texture()
 
 func on_card_input(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event.is_action_pressed("click"):
