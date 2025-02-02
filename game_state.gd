47a9b424b6584a8247570dfdf173305e6c12b1de
David Berkinsky <davidberkinsky@gmail.com>
2025-02-02 17:39:22 -0500

    fix bad message call

--- game_state.gd
+++ game_state.gd
@@ -7,7 +7,8 @@ func _ready() -> void:
 	reset()
 	
 func reset() -> void:
-	deck.reset()
+	# deck.reset()
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
